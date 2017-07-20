8705911104b8668682a795bc2a22de95ceda065f
lejohy <lejohy@gmail.com>
2017-07-20 14:19:17 +0900

    sentry support

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const cors = require('cors');
 const bunyan = require('bunyan');
+var Raven = require('raven');
 
 const app = express();
 const logger = bunyan.createLogger({
     name: 'nodeApp'
 });
+
+Raven.config(process.env.SENTRY_DSN).install();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -16,13 +19,19 @@ app.use(bodyParser.urlencoded({
 app.use(methodOverride());
 app.use(cors());
 
+app.use(Raven.requestHandler());
+
 app.get('/', (req, res) => {
     return res.status(200).json({
         status: 'running'
     });
 });
 
+app.get('/error', (req, res) => {
+    throw new Error('Borke!');
+});
 
+app.use(Raven.errorHandler());
 app.use((err, req, res, next) => {
     return res.status(500).send('500 Internal Server Error...');
 });
